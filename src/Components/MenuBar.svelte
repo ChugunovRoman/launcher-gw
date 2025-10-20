75415a8a28a7ca0e2d28a175b1e4655c4f95fcc8
ChugunovRoman <Zebs-BMK@yandex.ru>
2025-10-20 21:22:14 +0300

    impr: show point for selected meny item

--- src/Components/MenuBar.svelte
+++ src/Components/MenuBar.svelte
@@ -2,13 +2,36 @@
   import { Home, Settings, RunParams } from "../Icons";
 
   export let onSelect: (view: string) => void;
+
+  let activeItem = "home";
 </script>
 
 <div class="menubar">
-  <Home size={40} onclick={() => onSelect("home")} />
-  <RunParams size={40} onclick={() => onSelect("runParams")} />
+  <div class="baritem" class:active={activeItem === "home"}>
+    <Home
+      size={40}
+      onclick={() => {
+        onSelect("home");
+        activeItem = "home";
+      }} />
+  </div>
+  <div class="baritem" class:active={activeItem === "runParams"}>
+    <RunParams
+      size={40}
+      onclick={() => {
+        onSelect("runParams");
+        activeItem = "runParams";
+      }} />
+  </div>
   <div></div>
-  <Settings size={40} onclick={() => onSelect("settings")} />
+  <div class="baritem" class:active={activeItem === "settings"}>
+    <Settings
+      size={40}
+      onclick={() => {
+        onSelect("settings");
+        activeItem = "settings";
+      }} />
+  </div>
   <div></div>
 </div>
 
@@ -21,5 +44,21 @@
     background-color: rgba(0, 0, 0, 0);
     justify-items: anchor-center;
     height: 100%;
+  }
+
+  .baritem {
+    position: relative;
+    display: flex;
+    align-items: center;
+    padding-left: 16px; /* отступ для места под кружок */
+  }
+  .baritem.active::before {
+    content: "";
+    position: absolute;
+    left: 0;
+    width: 8px;
+    height: 8px;
+    border-radius: 50%;
+    background-color: white;
   }
 </style>
--- src/Icons/wrap.svelte
+++ src/Icons/wrap.svelte
@@ -3,17 +3,20 @@
   export let onclick = () => {};
 </script>
 
-<span role="button" tabindex="0" onclick={onclick}>
+<span role="button" tabindex="0" {onclick}>
   <slot />
 </span>
 
 <style>
   span {
-    display: inline-block;
+    display: flex;
+    align-items: center;
+    justify-content: center;
     transition: filter 0.15s ease;
   }
 
   span:hover {
     filter: drop-shadow(0 0 10px rgba(61, 93, 236, 0.993));
+    cursor: pointer;
   }
 </style>
